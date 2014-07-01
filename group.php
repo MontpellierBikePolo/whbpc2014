e24939cc8f0351631ae4efb25fe5d42d7ccd0f82
Romain <romain@exaprint.fr>
2014-07-01 22:39:35 +0200

    Commit template

---
+++ group.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<?php
+require 'common/head.html';
+?>
+<body id="index">
+<!-- Preloader -->
+<div id="mask">
+    <div id="loader"><img src="assets/img/preloader.gif" alt="preloader"/></div>
+</div>
+<header id="mobileheader" class="navigation-bar-header light visible-xs"></header>
+<?php
+require 'en/hero.html';
+require 'en/header.html';
+require 'en/stat.html';
+require 'en/schedule.html';
+require 'en/refereeing.html';
+require 'en/volunteers.html';
+require 'en/accomodations.html';
+require 'en/streaming.html';
+require 'en/sponsors.html';
+require 'en/registration.html';
+require 'en/map.html';
+require 'common/footer.html';
+?>
+</body>
